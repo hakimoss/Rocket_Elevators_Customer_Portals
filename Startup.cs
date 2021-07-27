05deacc5e65c603b692999ab0c7c4cc1c79af488
Abdelhakim Antar <hakimos@live.ca>
2021-07-27 10:33:49 -0400

    css et html fonctionne, manque image

--- Startup.cs
+++ Startup.cs
@@ -62,6 +62,7 @@ public class Startup
         }
         app.UseHttpsRedirection();
         app.UseStaticFiles();
+ 
 
         app.UseRouting();
 
---
+++ Views/Home/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Customer_Portal.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
